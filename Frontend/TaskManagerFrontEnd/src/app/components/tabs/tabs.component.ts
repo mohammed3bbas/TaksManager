c84af67a3f336896cf6d8e6631da822f5fed4319
Mohammad abbas <mohammed3bbas99@gmail.com>
2023-05-15 21:26:00 +0300

    Add basic tab component

--- Frontend/TaskManagerFrontEnd/src/app/components/tabs/tabs.component.ts
+++ Frontend/TaskManagerFrontEnd/src/app/components/tabs/tabs.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { TaskType } from 'src/app/models/entities/task-type';
+import { TaskTypeService } from './../../services/task-type.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.component.html',
   styleUrls: ['./tabs.component.scss']
 })
-export class TabsComponent {
+export class TabsComponent implements OnInit {
+
+  taskTypes: TaskType[] =[];
+
+  constructor(private TaskTypeService: TaskTypeService) { }
+
+  ngOnInit(): void {
+    this.TaskTypeService.getAllTaskTypes().subscribe((response: TaskType[]) => {
+      console.log(response);
+      this.taskTypes = response;
+    });
+  }
 
 }
