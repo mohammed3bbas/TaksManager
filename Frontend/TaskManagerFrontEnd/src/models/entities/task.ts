9371a830ee2c761b5abf644d42f9c156e8fe45a1
Mohammad abbas <mohammed3bbas99@gmail.com>
2023-05-15 12:45:15 +0300

    creare entities

---
+++ Frontend/TaskManagerFrontEnd/src/models/entities/task.ts
@@ -0,0 +1,9 @@
+import { TaskType } from './task-type';
+export interface Task {
+    id: number;
+    name: string;
+    description: string;
+    dueDate: Date;
+    creationDate: Date;
+    taskType: TaskType;
+}
